44dc3bb7200a84625c8edaaa019f2ed3eb3ab0a6
Jake Gill <jake.w.g21@gmail.com>
2024-03-15 17:27:01 -0400

    Made  adocker container.
    Dont forget .env

--- src/config/constants.config.ts
+++ src/config/constants.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { Secret } from "jsonwebtoken";
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 8000;
 export const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
 export const CATALOG_URI = process.env.CATALOG_URI;
 export const MONGO_DB = process.env.MONGO_DB || "test";
